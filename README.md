f06d62ef470020cd929c429346f0949c18267643
Mat <vecch.matteo@gmail.com>
2021-01-09 20:05:39 +0100

    Project initiation

---
+++ README.md
@@ -0,0 +1,59 @@
+<br />
+<p align="center">
+  <img src="resources/icon.png" alt="Logo" width="80" height="80">
+
+  <h3 align="center">Timetable Viewer Mobile</h3>
+
+  <p align="center">
+    Application for view your university timetable.
+    <br />
+    <a href="https://github.com/Matunknown/timetable-viewer-mobile/releases"><strong>Download »</strong></a>
+</p>
+
+## About The Project
+
+An application to consult his timetable for computer science students at the University of Evry.
+
+### Built With
+
+* [Ionic](https://ionicframework.com/)
+
+## Getting Started
+
+### Prerequisites
+
+* npm
+  ```sh
+  npm install npm@latest -g
+  ```
+
+* Ionic CLI
+  ```sh
+  npm install -g @ionic/cli
+  ```
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/Matunknown/timetable-viewer-mobile.git
+   ```
+2. Install NPM packages
+   ```sh
+   npm install
+   ```
+3. Run
+   ```sh
+   ionic serve
+   ```
+
+### Building Distributables
+
+* Generate an APK
+    ```sh
+    ionic cordova build android --prod --release
+    ```
+
+## License
+
+Distributed under the MIT License.
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PickerController } from '@ionic/angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,96 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  code = 68426749;
+  weekNumber = this.getWeekNumber();
+  year = (new Date().getMonth() === 0 && this.weekNumber >= 52) ? new Date().getFullYear() - 1 : new Date().getFullYear();
+
+  sliderOpt = {
+    zoom: {
+      maxRatio: 2
+    }
+  };
+
+  constructor(private pickerController: PickerController, private storage: Storage) {
+    this.getCodeData();
+  }
+
+  getCodeData() {
+    this.storage.get('code').then((val) => {
+      console.log('Code is', val);
+      this.code = val;
+    });
+  }
+
+  getWeekNumber() {
+    const d = new Date(Date.UTC(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()));
+    const dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil((((Number(d) - Number(yearStart)) / 86400000) + 1) / 7);
+  }
+  
+  btnToday() {
+    this.weekNumber = this.getWeekNumber();
+  }
+
+  btnBack() {
+    this.weekNumber --;
+  }
+
+  btnForward() {
+    this.weekNumber ++;
+  }
+
+  async showPicker() {
+    let picker = await this.pickerController.create({
+      buttons: [
+        {
+          text:'Ok'
+        }
+      ],
+      columns:[{
+        name:'groupe',
+        options: [
+          {text: 'M2 SA', value: '68419887'},
+          {text: 'M2 SR', value: '68419888'},
+          {text: 'M2 MIAGE I.', value: '68419883'},
+          {text: 'M2 MIAGE A.', value: '6'},
+
+          {text: 'M1 INFO G1', value: '68425740'},
+          {text: 'M1 INFO G2', value: '68425741'},
+          {text: 'M1 INFO SA', value: '68426833'},
+          {text: 'M1 INFO SR', value: '68426834'},
+          {text: 'M1 MIAGE I.', value: '68419919'},
+          {text: 'M1 MIAGE A.', value: '68419886'},
+
+          {text: 'L3 SA', value: '68419893'},
+          {text: 'L3 SR', value: '68419892'},
+          {text: 'L3 MIAGE I.', value: '68419891'},
+          {text: 'L3 MIAGE A.', value: '7'},
+          {text: 'L3 DOUBLE', value: '68426751'},
+
+          {text: 'L2 INFO G1', value: '68426361'},
+          {text: 'L2 INFO G2', value: '68426362'},
+          {text: 'L2 INFO G3', value: '68426891'},
+          {text: 'L2 INFO G4', value: '68426987'},
+
+          {text: 'L1 MI G1', value: '18'},
+          {text: 'L1 MI G2', value: '8'},
+          {text: 'L1 MI G3', value: '68426495'},
+          {text: 'L1 MI G4', value: '68426747'},
+          {text: 'L1 MI G5', value: '68426748'},
+          {text: 'L1 MI G6', value: '68426749'},
+          {text: 'L1 SDVI', value: '68424877'}
+        ]
+      }]
+    });
+    picker.present();
+    picker.onDidDismiss().then(async data => {
+      let col = await picker.getColumn('groupe');
+      this.code = col.options[col.selectedIndex].value;
+      this.storage.set('code', this.code);
+    });
+  }
 
 }
